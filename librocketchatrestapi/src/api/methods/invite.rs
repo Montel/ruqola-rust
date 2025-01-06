2c4567a43a2ab1f5500998507910f9f2ec5ae9bf
Laurent Montel <laurent@kdab.com>
2025-01-06 20:21:44 +0100

    Update copyright

--- librocketchatrestapi/src/api/methods/invite.rs
+++ librocketchatrestapi/src/api/methods/invite.rs
@@ -1,5 +1,5 @@
 /*
-* SPDX-FileCopyrightText: 2023-2024 Laurent Montel <laurent.montel@kdab.com>
+* SPDX-FileCopyrightText: 2023-2025 Laurent Montel <laurent.montel@kdab.com>
 *
 * SPDX-License-Identifier: LGPL-2.0-or-later
 */
--- libruqolacore/src/languages/downloadappslanguages.rs
+++ libruqolacore/src/languages/downloadappslanguages.rs
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2024 Laurent Montel <laurent.montel@kdab.com>
+ * SPDX-FileCopyrightText: 2024-2025 Laurent Montel <laurent.montel@kdab.com>
  *
  * SPDX-License-Identifier: LGPL-2.0-or-later
  */
