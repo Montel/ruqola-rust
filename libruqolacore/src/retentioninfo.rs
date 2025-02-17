9c33f2f2c9ada99592aa5ccaf885b158405e0cd2
Laurent Montel <laurent@kdab.com>
2025-02-17 13:53:13 +0100

    Start to implement retentioninfo support

---
+++ libruqolacore/src/retentioninfo.rs
@@ -0,0 +1,68 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Laurent Montel <laurent.montel@kdab.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+use serde::Deserialize;
+
+#[derive(Clone, Deserialize, Debug, PartialEq)]
+pub struct RetentionInfo {
+    #[serde(rename = "maxAge")]
+    pub max_age: i64,
+    pub enabled: bool,
+    #[serde(rename = "overrideGlobal")]
+    pub override_global: bool,
+    #[serde(rename = "excludePinned")]
+    pub exclude_pinned: bool,
+    #[serde(rename = "filesOnly")]
+    pub files_only: bool,
+}
+
+impl Default for RetentionInfo {
+    fn default() -> Self {
+        RetentionInfo::new()
+    }
+}
+
+impl RetentionInfo {
+    pub fn new() -> Self {
+        RetentionInfo {
+            max_age: -1,
+            enabled: false,
+            override_global: false,
+            exclude_pinned: false,
+            files_only: false,
+        }
+    }
+    pub fn parse_elements(&mut self, json: &str) {
+        if let Ok(val) = serde_json::from_str::<RetentionInfo>(json) {
+            *self = val
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+
+    use crate::retentioninfo::RetentionInfo;
+
+    // For test !
+    pub fn parse(filename: &str) -> RetentionInfo {
+        let file = File::open(filename).expect("Failed to open file");
+        serde_json::from_reader(file).expect("JSON was not well-formatted")
+    }
+
+    #[test]
+    fn test_is_empty() {
+        let b: RetentionInfo = RetentionInfo::new();
+    }
+    #[test]
+    fn test_parsing() {
+        {
+            // Load file
+            //let permission = parse("src/data/RetentionInfo/RetentionInfo.json");
+        }
+    }
+}
