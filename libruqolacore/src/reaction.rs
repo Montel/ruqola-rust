2933763f3e00066d82fb247543d0f363cd4d7913
Laurent Montel <laurent@kdab.com>
2025-02-19 13:42:56 +0100

    Implement converted_users_name_as_tooltip

--- libruqolacore/src/reaction.rs
+++ libruqolacore/src/reaction.rs
@@ -25,4 +25,63 @@ impl Reaction {
             user_names: Vec::new(),
         }
     }
+
+    pub fn converted_users_name_as_tooltip(self) -> String {
+        let result = String::from("");
+        if self.user_names.is_empty() {
+            result
+        } else if self.user_names.len() == 1 {
+            let initial_value = self.user_names[0].clone();
+            let reaction_name = self.reaction_name;
+            return format!("{initial_value} reacted with {reaction_name}");
+        } else {
+            let mut notification_str = String::from("");
+            let total = self.user_names.len();
+            for i in 0..total {
+                let user = self.user_names[i].clone();
+                if i == 0 {
+                    notification_str = user;
+                } else if i < (total - 1) {
+                    notification_str = format!("{notification_str}, {user}");
+                } else {
+                    notification_str = format!("{notification_str} and {user}");
+                }
+            }
+            let reaction_name = self.reaction_name;
+            return format!("{notification_str} reacted with {reaction_name}");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::reaction::Reaction;
+
+    #[test]
+    fn test_tooltip() {
+        let mut reaction: Reaction = Reaction::new();
+        reaction.reaction_name = ":foo:".to_string();
+        assert!(reaction
+            .clone()
+            .converted_users_name_as_tooltip()
+            .is_empty(),);
+
+        reaction.user_names = vec!["bla".to_string()];
+        assert_eq!(
+            reaction.clone().converted_users_name_as_tooltip(),
+            "bla reacted with :foo:"
+        );
+
+        reaction.user_names = vec!["bla".to_string(), "blo".to_string()];
+        assert_eq!(
+            reaction.clone().converted_users_name_as_tooltip(),
+            "bla and blo reacted with :foo:"
+        );
+
+        reaction.user_names = vec!["bla".to_string(), "blo".to_string(), "bli".to_string()];
+        assert_eq!(
+            reaction.clone().converted_users_name_as_tooltip(),
+            "bla, blo and bli reacted with :foo:"
+        );
+    }
 }
