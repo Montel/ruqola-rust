2fe60a78ed97d97e0b2570f6fa75a59879534304
Laurent Montel <laurent@kdab.com>
2025-03-13 13:14:46 +0100

    Add autotest for parsing personalaccesstokeninfos

--- libruqolacore/src/permissionaccesstokeninfos.rs
+++ libruqolacore/src/permissionaccesstokeninfos.rs
@@ -9,6 +9,7 @@ use serde::Deserialize;
 
 #[derive(Clone, Deserialize, Debug, PartialEq)]
 pub struct PermissionAccessTokenInfos {
+    #[serde(rename = "tokens")]
     pub permission_access_token_infos: Vec<PermissionAccessTokenInfo>,
 }
 
@@ -35,7 +36,9 @@ impl PermissionAccessTokenInfos {
 mod tests {
     use std::fs::File;
 
+    use crate::permissionaccesstokeninfo::PermissionAccessTokenInfo;
     use crate::permissionaccesstokeninfos::PermissionAccessTokenInfos;
+    use chrono::{DateTime, Utc};
 
     // For test !
     pub fn parse(filename: &str) -> PermissionAccessTokenInfos {
@@ -47,5 +50,65 @@ mod tests {
     fn test_is_empty() {
         let b = PermissionAccessTokenInfos::new();
         assert!(b.permission_access_token_infos.is_empty());
+    }
+
+    #[test]
+    fn test_load_empty() {
+        let b = parse("src/data/personalaccesstokeninfos/empty-token.json");
+        assert!(b.permission_access_token_infos.is_empty());
+    }
+
+    #[test]
+    fn test_load_four_tokens() {
+        let b = parse("src/data/personalaccesstokeninfos/four-tokens.json");
+        assert!(!b.permission_access_token_infos.is_empty());
+        let mut lst: Vec<PermissionAccessTokenInfo> = Vec::new();
+        {
+            let mut i = PermissionAccessTokenInfo::new();
+            i.bypass_two_factor = false;
+            i.last_token_part = String::from("I6mHG3");
+            i.name = String::from("test1");
+            i.create_date_time = DateTime::<Utc>::from_utc(
+                chrono::NaiveDateTime::from_timestamp_millis(1741856600248).unwrap(),
+                Utc,
+            );
+            lst.push(i);
+        }
+        {
+            let mut i = PermissionAccessTokenInfo::new();
+            i.bypass_two_factor = false;
+            i.last_token_part = String::from("ClGA8G");
+            i.name = String::from("test2");
+            i.create_date_time = DateTime::<Utc>::from_utc(
+                chrono::NaiveDateTime::from_timestamp_millis(1741856613045).unwrap(),
+                Utc,
+            );
+
+            lst.push(i);
+        }
+        {
+            let mut i = PermissionAccessTokenInfo::new();
+            i.bypass_two_factor = false;
+            i.last_token_part = String::from("eM7RyS");
+            i.name = String::from("test3");
+            i.create_date_time = DateTime::<Utc>::from_utc(
+                chrono::NaiveDateTime::from_timestamp_millis(1741856625084).unwrap(),
+                Utc,
+            );
+
+            lst.push(i);
+        }
+        {
+            let mut i = PermissionAccessTokenInfo::new();
+            i.bypass_two_factor = true;
+            i.last_token_part = String::from("nGVprC");
+            i.name = String::from("test4");
+            i.create_date_time = DateTime::<Utc>::from_utc(
+                chrono::NaiveDateTime::from_timestamp_millis(1741856839672).unwrap(),
+                Utc,
+            );
+            lst.push(i);
+        }
+        assert_eq!(b.permission_access_token_infos, lst);
     }
 }
