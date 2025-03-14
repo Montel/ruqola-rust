32b202d59b3b9d73b27dfb816b9aac0a6dd25130
Laurent Montel <laurent@kdab.com>
2025-03-14 20:00:02 +0100

    Improve messageinfo

--- libruqolacore/src/messageinfo.rs
+++ libruqolacore/src/messageinfo.rs
@@ -6,6 +6,99 @@
 
 use serde::Deserialize;
 use std::fmt;
+#[derive(Default, Clone, Deserialize, Debug, PartialEq)]
+pub enum SystemMessageType {
+    #[default]
+    Unknown,
+    #[serde(alias = "uj")]
+    UserJoined,
+    #[serde(alias = "ul")]
+    UserLeft,
+    #[serde(alias = "ult")]
+    UserLeftTeam,
+    #[serde(alias = "room_changed_topic")]
+    RoomTopicChanged,
+    #[serde(alias = "au")]
+    UserAdded,
+    #[serde(alias = "r")]
+    RoomNameChanged,
+    #[serde(alias = "ru")]
+    UserRemoved,
+    #[serde(alias = "room_changed_description")]
+    RoomDescriptionChanged,
+    #[serde(alias = "room_changed_announcement")]
+    RoomAnnoucementChanged,
+    #[serde(alias = "room_changed_privacy")]
+    RoomPrivacyChanged,
+    #[serde(alias = "jitsi_call_started")]
+    JitsiCallStarted,
+    #[serde(alias = "rm")]
+    MessageDeleted,
+    #[serde(alias = "message_pinned")]
+    Pinned,
+    #[serde(alias = "otr")]
+    EncryptedMessage,
+    #[serde(alias = "user-muted")]
+    UserMuted,
+    #[serde(alias = "user-unmuted")]
+    UserUnmuted,
+    #[serde(alias = "subscription-role-added")]
+    SubscriptionRoleAdded,
+    #[serde(alias = "subscription-role-removed")]
+    SubscriptionRoleRemoved,
+    #[serde(alias = "e2e")]
+    MessageE2E,
+    #[serde(alias = "discussion-created")]
+    DiscussionCreated,
+    #[serde(alias = "ut")]
+    UserJoinedConversation,
+    #[serde(alias = "room-archived")]
+    RoomArchived,
+    #[serde(alias = "room-unarchived")]
+    RoomUnarchived,
+    #[serde(alias = "rtc")]
+    Rtc,
+    #[serde(alias = "wm")]
+    Welcome,
+    #[serde(alias = "room_changed_avatar")]
+    RoomAvatarChanged,
+    #[serde(alias = "room_e2e_enabled")]
+    RoomE2eEnabled,
+    #[serde(alias = "room_e2e_disabled")]
+    RoomE2eDisabled,
+    #[serde(alias = "room-set-read-only")]
+    RoomSetReadOnly,
+    #[serde(alias = "room-removed-read-only")]
+    RoomRemoveReadOnly,
+    #[serde(alias = "added-user-to-team")]
+    AddedUserToTeam,
+    #[serde(alias = "removed-user-from-team")]
+    RemovedUserFromTeam,
+    #[serde(alias = "user-converted-to-team")]
+    UserConvertedToTeam,
+    #[serde(alias = "user-converted-to-channel")]
+    UserConvertedToChannel,
+    #[serde(alias = "user-removed-room-from-team")]
+    UserRemovedRoomFromTeam,
+    #[serde(alias = "user-deleted-room-from-team")]
+    UserDeletedRoomFromTeam,
+    #[serde(alias = "user-added-room-to-team")]
+    UserAddedRoomToTeam,
+    #[serde(alias = "room-allowed-reacting")]
+    RoomAllowedReacting,
+    #[serde(alias = "room-disallowed-reacting")]
+    RoomDisallowedReacting,
+    #[serde(alias = "ujt")]
+    UserJoinedTeam,
+    #[serde(alias = "user_joined_otr")]
+    UserJoinedOtr,
+    #[serde(alias = "user_key_refreshed_successfully")]
+    UserKeyRefreshedSuccessfully,
+    #[serde(alias = "user_requested_otr_key_refresh")]
+    UserRequesterOtrKeyRefresh,
+    #[serde(alias = "videoconf")]
+    VideoConf,
+}
 
 #[allow(non_snake_case)]
 #[derive(Clone, Deserialize, Debug, PartialEq)]
@@ -28,6 +121,8 @@ pub struct MessageInfo {
     pub dcount: i64,
     // TODO add type
     pub rid: String,
+
+    pub unread: bool,
 }
 
 impl Default for MessageInfo {
@@ -50,6 +145,7 @@ impl MessageInfo {
             rid: String::default(),
             tcount: 0,
             dcount: 0,
+            unread: false,
         }
     }
     pub fn parse(filename: &str) -> MessageInfo {
