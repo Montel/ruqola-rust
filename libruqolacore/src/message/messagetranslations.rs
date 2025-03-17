8bd370f330c5751531136aad6e2c70d800dbba46
Laurent Montel <laurent@kdab.com>
2025-03-17 13:37:42 +0100

    Add  message translation

--- libruqolacore/src/message/messagetranslations.rs
+++ libruqolacore/src/message/messagetranslations.rs
@@ -4,13 +4,43 @@
  * SPDX-License-Identifier: LGPL-2.0-or-later
  */
 
-use std::collections::HashMap;
-
 use serde::Deserialize;
+use std::collections::HashMap;
+use std::fmt;
 #[derive(Default, Clone, Deserialize, Debug, PartialEq)]
 pub struct MessageTranslations {
     #[serde(rename = "translations")]
     pub translated_string: HashMap<String, String>,
 }
 
-// TODO add test
+impl MessageTranslations {
+    pub fn new() -> Self {
+        MessageTranslations {
+            translated_string: HashMap::new(),
+        }
+    }
+}
+
+/*
+Debug output for MessageTranslations
+*/
+impl fmt::Display for MessageTranslations {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "MessageTranslations(translated_string: {:?})",
+            self.translated_string
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::message::messagetranslations::MessageTranslations;
+
+    #[test]
+    fn test_is_empty() {
+        let b = MessageTranslations::new();
+        assert!(b.translated_string.is_empty());
+    }
+}
