3bc7309054fbf7fb0ebeb1169c6ad0dbfc8018fa
Laurent Montel <laurent@kdab.com>
2025-04-03 18:45:09 +0200

    Add servicepassword

--- libruqolacore/src/ownuser/servicepassword.rs
+++ libruqolacore/src/ownuser/servicepassword.rs
@@ -5,3 +5,49 @@
  */
 
 use serde::Deserialize;
+
+#[derive(Clone, Deserialize, Debug, PartialEq)]
+pub struct ServicePassword {
+    pub password: String,
+    pub email2_fa_enabled: bool,
+    pub totp: bool,
+}
+
+impl Default for ServicePassword {
+    fn default() -> Self {
+        ServicePassword::new()
+    }
+}
+
+impl ServicePassword {
+    pub fn new() -> Self {
+        ServicePassword {
+            password: String::default(),
+            email2_fa_enabled: false,
+            totp: false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ownuser::servicepassword::ServicePassword;
+    use std::fs::File;
+
+    // Using by test
+    pub fn parse(filename: &str) -> ServicePassword {
+        let file = File::open(filename).expect("Failed to open file");
+        serde_json::from_reader(file).expect("JSON was not well-formatted")
+    }
+
+    #[test]
+    fn test_parsing() {
+        {
+            // Load file
+            // TODO
+            //let preferences = parse("src/data/ownuserpreferences/ownuserpreferences1.json");
+            //assert_eq!(permission.update.len(), 1147);
+            //assert_eq!(permission.remove.len(), 0);
+        }
+    }
+}
